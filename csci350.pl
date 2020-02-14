27ae0f9d08be582a59518c6580685547046dac66
Genesis <genesis.smothers@bison.howard.edu>
2020-02-13 21:32:05 -0500

    Helper Functions
    I created my remove function that will remove the characters it finds in the list

--- csci350.pl
+++ csci350.pl
@@ -4,4 +4,10 @@
 append([], X, X).
 append([X | Y], Z, [X | W]) :-
 	append(Y,Z,W). 
+	
+%Remove Characters Function 
+remove([], List, List). 
+remove([Head | Tail], L2, Return) :-
+	not(number(Head)) -> remove(Tail, L2, Return);
+	number(Head) -> remove(Tail, [Head | L2], Return). 
 
